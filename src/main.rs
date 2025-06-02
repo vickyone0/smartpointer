becc95676325f099aa9684637b845016e710f6fe
vickyone0 <vigneshs0.618@gmail.com>
2025-06-02 07:47:15 -0400

    refcell

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
-enum List {
-    Cons(i32, Rc<List>),
-    Nil,
+pub trait Messenger {
+    fn send(&self, msg: &str);
 }
-use List::{Cons, Nil};
-use std::rc::Rc;
 
-fn main(){
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(List::Nil)))));
-
-    println!("a has {} strong references", Rc::strong_count(&a));
-
-    let b = Rc::new(List::Cons(3,Rc::clone(&a)));
-
-    println!("a has {} strong references", Rc::strong_count(&a));
-    {
-    let c = Rc::new(List::Cons(4, Rc::clone(&a)));
-    println!("a has {} strong references", Rc::strong_count(&a));
-
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+impl<'a, T: Messenger> LimitTracker<'a, T> {
+    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
     }
-    println!("a has {} strong references after c out of scope", Rc::strong_count(&a));
 
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+        let percentage = self.value as f64 / self.max as f64;
+
+        if percentage >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        } else if percentage >= 0.9 {
+            self.messenger.send("Urgent warning: You've used up over 90% of your quota!");
+        } else if percentage >= 0.75 {
+            self.messenger.send("Warning: You've used up over 75% of your quota!");
+        }
+    }
+}
+fn main() {
+    
 }
