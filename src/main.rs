55920ec49641ebcf5f4e30bbdb9d757a67ff318e
vickyone0 <vigneshs0.618@gmail.com>
2025-06-03 00:01:18 -0400

    Rc<RefCell<>>

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,31 @@
-fn main() {}
+use std::{cell::{Ref, RefCell}, rc::Rc};
+use List::{Cons, Nil};
+
+
+fn main() {
+
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+   
+    println!("a: {:?}", a);
+    println!("b: {:?}", b);
+    println!("c: {:?}", c);
+    println!("value = {}", value.borrow());
+     *value.borrow_mut() += 10;
+    println!("a: {:?}", a);
+    println!("b: {:?}", b);
+    println!("c: {:?}", c);
+}
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+
+
